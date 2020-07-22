b1939e3f2e266357d56ddceb722e4ba5e823e91a
Ricardo Laranjeira <ricardo.laranjeira.74@gmail.com>
2020-07-22 18:50:44 -0400

    Primeiro Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,12 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <h3>Ricardo Laranjeira</h3>
+      <p>
+        <a href='mailto:suporte@caceresvirtual.com.br'>
+          suporte@caceresvirtual.com.br
+        </a>{' '}
+        - (65) 99611 7368
+      </p>
     </div>
   )
 }
